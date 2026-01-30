552bb7e63a689ec1e3239c5785f3fc178c6124d0
Maheesha <maheeshamihiran2021@gmail.com>
2026-01-30 13:29:44 +0530

    Fix syntax error in outputs.tf - remove extra closing brace

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -9,7 +9,6 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_eip.app_server.public_ip
 }
-}
 
 output "instance_public_dns" {
   description = "Public DNS of the EC2 instance"
